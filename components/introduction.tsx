2a27b33d550924b146a13d6c0fa4b7d1bfa8bd22
ledexxx <leedex96@gmail.com>
2024-07-25 01:28:22 -0400

    cambios responsive

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -36,7 +36,7 @@ const Introduction = () => {
                         </a>
                         <a href="/contact"
                             className="px-3 py-2 my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary" >
-                            Contactame
+                            Contáctame
                         </a>
                     </div>
                 </div>
