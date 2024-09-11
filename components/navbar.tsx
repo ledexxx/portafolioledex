5d3a3776c28ce16cfd9463f6b4f7ff1ee4264cb4
ledexxx <leedex96@gmail.com>
2024-09-11 11:39:44 -0300

    cambio navbar2

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,9 +8,9 @@ const Navbar = () => {
     const router = usePathname();
     
     return (
-        <MotionTransition position="right" className="fixed z-40 flex flex-col items-center justify-center w-full mt-auto pt-4 h-max bottom-10 shadow-sm">
+        <MotionTransition position="right" className="fixed z-40 flex flex-col items-center justify-center w-full mt-auto pt-4 h-max bottom-10 ">
             <nav>
-                <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/25S background-blur-sm ">
+                <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/25 background-blur-sm ">
                     {itemsNavbar.map((item) => (
                         <div key={item.id}
                             className={`px-3 py-2 transition duration-100 rounded-full cursor-pointer hover:bg-tertiary ${router === item.link && 'bg-tertiary'}`}
